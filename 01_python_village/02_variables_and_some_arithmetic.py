c816ca066ca510f8377bf5b82d7bc13133baa7b9
tmyroy <tmyroy@proton.me>
2024-11-09 13:40:19 +0100

    add shebang to python village scripts

--- 01_python_village/02_variables_and_some_arithmetic.py
+++ 01_python_village/02_variables_and_some_arithmetic.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Given: Two positive integers a and b, each less than 0
 # Return: Integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
 
--- 01_python_village/06_dictionaries.py
+++ 01_python_village/06_dictionaries.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Given: A string s of length at most 10000 letters. 
 # Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order
 
@@ -18,7 +20,7 @@ def word_count(s):
     return '\n'.join(f"{word} {count}" for word, count in word_counts.items())
 
 
-s = "We tried list and we tried dicts also we tried Zen"
+s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
 
 print(f"Word Count with Counter\n{'-' * 25}\n{word_count_with_counter(s)}")
 print(f"\n\nWord Count\n{'-' * 10}\n{word_count(s)}")
