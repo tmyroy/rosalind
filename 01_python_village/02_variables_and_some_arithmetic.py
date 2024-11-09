5af30dfbfe000c4c78421d69aacedf2f75b520e0
tmyroy <tmyroy@proton.me>
2024-11-09 13:06:24 +0100

    02: solve exercise variables and some arithemtic

---
+++ 01_python_village/02_variables_and_some_arithmetic.py
@@ -0,0 +1,12 @@
+# Given: Two positive integers a and b, each less than 0
+# Return: Integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
+
+def hypotenuse_square(a, b):
+    return a**2 + b**2
+
+
+a = 915
+b = 848
+print(hypotenuse_square(a, b))
+
